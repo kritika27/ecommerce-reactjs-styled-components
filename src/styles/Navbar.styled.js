55401077d226c234fd0e37e60cb9f6d86030d1a7
Kritika Srivastava <kritikasri27@gmail.com>
2023-02-02 01:43:42 +0530

    Created files for styling using styled-components

---
+++ src/styles/Navbar.styled.js
@@ -0,0 +1,29 @@
+import  styled  from "styled-components";
+
+export const Nav=styled.div`
+display:flex;
+justify-content:space-between;
+align-items:center;
+height:10vh;
+background-color:#FFF;
+font-family:cursive;
+padding:0px 15px 0px 15px;
+`;
+export const Logo=styled.h1`
+color:#3AAFA9;
+font-weight:bold;
+`;
+
+export const Menu=styled.ul`
+list-style-type:none;
+display:flex;
+color:#444;
+font-family:Segoe UI;
+`;
+
+export const Item=styled.li`
+margin:5px;
+font-weight:500;
+font-size:18px;
+`;
+
