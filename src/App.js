b92e4b90833142e07c9b7359fd186799372a8fdd
Kritika Srivastava <kritikasri27@gmail.com>
2023-02-02 03:39:45 +0530

    Added dummy Products page route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
+import { Products } from "./components/Products";
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
 
       <Route  path="/" element={<Home/>}/>
+      <Route  path="/products" element={<Products/>}/>
       </Routes>    
       
     </Router>
