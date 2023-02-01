66d1b64a98ea2e16118bbd342f73ab70d70accff
Kritika Srivastava <kritikasri27@gmail.com>
2023-02-02 03:08:34 +0530

    Created Homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Homepage,Pic,Content,Header,SmallText,Head,Button } from '../../styles/Home.styled';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Homepage>
+      <Content>
+        <Head>
+        <Header>Ready for new stuff</Header>
+        <SmallText>Buy new stock at reasonable cost</SmallText>
+        <Button>Get Started</Button>
+        </Head>
+      </Content>
+      
+      
+<Pic></Pic>
+
+    </Homepage>
   )
 }
 
